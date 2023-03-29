4271ebcf0c362e321bb2351f54bc64324931e50c
CesarLopezz <cesar.lopez@correo.unimet.edu.ve>
2023-03-29 14:42:50 -0400

    Cambios
    cambios

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 
+
 def codigoUnico(db: list, modelo: str) -> bool:
     for juego in db:
         if juego["modelo"] == modelo:
             return False
     return True
 
+def mostrar_juego(game):
+        print(f'''
+    ================ {game["titulo"]} ===================
+
+    Modelo --> {game["modelo"]}  
+    Nombre del juego --> {game["titulo"]}
+    Precio de alquiler --> {game["precio"]}Bs
+    Estatus del juego --> {game["status"]}
+    Codigo Hash --> 
+
+    =====================================================
+        ''')
 
 def agregar_juego(db: list):
-    # print("Agregar un juego")
+    
     titulo = input("Ingrese el nombre del juego \n -->")
 
     while len(titulo) > 10:
-        titulo = input("Ingrese el nombre del juego otra vez \n -->")
+        titulo = input("Ingrese el nombre del juego otra vez (SOLO MAXIMO 10 CARACTERES) \n -->")
 
     modelo = input("Ingrese el modelo (Formato = AAAAAA00) \n -->")
 
@@ -24,14 +37,14 @@ def agregar_juego(db: list):
         modelo = input(
             "Ingrese el modelo otra vez (Formato = AAAAAA00) \n -->")
 
-    precio = input("Ingrese el precio del juego \n -->")
-
+    precio = input("Ingrese el precio del juego (Monto ENTERO hasta 999bs) \n -->")
+    
     while (
         not precio.isnumeric() or
         int(precio) <= 0 or
         int(precio) > 999
     ):
-        precio = input("Ingrese el precio del juego otra vez \n -->")
+        precio = input("Ingrese el precio del juego otra vez (Monto ENTERO hasta 999bs) \n -->")
 
     db.append(
         {
@@ -43,33 +56,18 @@ def agregar_juego(db: list):
     )
     print("Juego agregado correctamente")
 
-
 def searchByModel(db: list, model: str) -> dict:
     for game in db:
         if game["modelo"] == model:
-            if game["status"] == "EN STOCK":
-                return game
-            else:
-                print("Ese juego ya esta alquilado")
+            return game
     return {}
 
 
 def searchByTitle(db: list, title: str) -> dict:
     for game in db:
         if game["titulo"] == title:
-            if game["status"] == "EN STOCK":
-                return game
-            else:
-                print("Ese juego ya esta alquilado")
+            return game
     return {}
-
-
-def writeDataBase(db: list):
-    with open("database.txt", "w") as file:
-        for game in db:
-            file.write(
-                f"{game['titulo']},{game['modelo']},{game['precio']},{game['status']}")
-
 
 def rentAGame(db: list):
     while True:
@@ -87,42 +85,15 @@ def rentAGame(db: list):
 
         game = {}
         if (option == "1"):
-            game = searchByModel(
-                db, input("Ingrese el modelo del juego \n -->"))
-
-            while game == {}:
-                print("Ese juego no existe o ya esta alquilado")
-                game = searchByModel(
-                    db, input("Ingrese el modelo del juego otra vez \n -->"))
-
-            titulo = game["titulo"]
-
-            q = input(
-                f"seguro que quiere alquilar {titulo} ?\n 1. si\n 2.no\n introduzca su seleccion:")
-            if q == "1":
-                game["status"] = "ALQUILADO"
-                print(f"{titulo} ha sido alquilado con exito")
-
-        elif option == "2":
-            game = searchByTitle(
-                db, input("Ingrese el titulo del juego \n -->"))
-
-            while game == {}:
-                print("Ese juego no existe o ya esta alquilado")
-                game = searchByTitle(
-                    db, input("Ingrese el titulo del juego otra vez \n -->"))
-
-            titulo = game["titulo"]
-            q = input(
-                f"seguro que quiere alquilar {titulo} ?\n 1.Si\n 2.No\n introduzca su seleccion:")
-            if q == "1":
-                game["status"] = "ALQUILADO"
-                print(f"{titulo} ha sido alquilado con exito")
+            game = searchByModel(db, input("Ingrese el modelo del juego \n -->"))
+            mostrar_juego(game)
+        elif option == "2" :
+            game = searchByTitle(db, input("Ingrese el titulo del juego \n -->"))
+            mostrar_juego(game)
         elif option == "3":
             break
         else:
             print("Ingreso incorrecto, vuelvalo a intentar.")
-
 
 def eliminar_juego(db: list):
     print("Eliminar un juego")
@@ -134,21 +105,9 @@ def eliminar_juego(db: list):
             return
     print("No se encontro el juego")
 
-
-def devolver_juego(db: list):
-    print("Devolver un juego")
-    modelo = input("ingrese el modelo del juego \n -->")
-    for juego in db:
-        if juego["modelo"] == modelo:
-            if juego["status"] == "ALQUILADO":
-                juego["status"] == "EN STOCK"
-                print("juego devuelto exitosamente")
-                return
-    print("ese juego no se encuentra alquilado")
-
-
 def main():
-    db = []
+    
+    db = [ ]
     while True:
         print(f"""
         ===========Rent-A-Game============
@@ -171,7 +130,7 @@ def main():
         elif option == "3":
             rentAGame(db)
         elif option == "4":
-            devolver_juego(db)
+            pass
         elif option == "5":
             eliminar_juego(db)
         elif option == "6":
